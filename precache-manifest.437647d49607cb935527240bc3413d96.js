41b27739e5eb351d5da47fd3da1cde1ee089c84b
Wanderson Alves Ferreira <wandersonalvesferreira2012@gmail.com>
2018-06-21 17:58:07 -0300

    Updates

---
+++ precache-manifest.437647d49607cb935527240bc3413d96.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "db70d0b9cb27ada1a260a2b35e756b8b",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-400italic.db70d0b9.woff"
+  },
+  {
+    "revision": "cf05a7ee512b69debdfd",
+    "url": "/vue-shadow-badge/css/chunk-vendors.5f8e7e9a.css"
+  },
+  {
+    "revision": "8a648ff38ded89ea15916e84529d62d3",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-300italic.8a648ff3.woff"
+  },
+  {
+    "revision": "9bd491317b8eb397152d",
+    "url": "/vue-shadow-badge/js/app.9bd49131.js"
+  },
+  {
+    "revision": "c02f5da6e82e1efe0b45841bfd49ce37",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-700italic.c02f5da6.woff2"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "987032ea5d57c93d8da215678eae3b86",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-400italic.987032ea.woff2"
+  },
+  {
+    "revision": "72e19cbb0e38c6773a7751156752cec4",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-700italic.72e19cbb.woff"
+  },
+  {
+    "revision": "4950a7205f0b5cefe41fc03ac346e236",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-600italic.4950a720.woff2"
+  },
+  {
+    "revision": "223a277bd88d8a90c8cdf24cda0ad5f5",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-600.223a277b.woff2"
+  },
+  {
+    "revision": "318ea1ada4102c0704a0637228dcad03",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-600italic.318ea1ad.woff"
+  },
+  {
+    "revision": "79b58175343190550489efe46a7f1138",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-800italic.79b58175.woff"
+  },
+  {
+    "revision": "cf05a7ee512b69debdfd",
+    "url": "/vue-shadow-badge/js/chunk-vendors.cf05a7ee.js"
+  },
+  {
+    "revision": "6b3973ffe02bb6a8be0f8453506ec032",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-800italic.6b3973ff.woff2"
+  },
+  {
+    "revision": "06bbd3188b34820cd83a0e0b3d0a6f57",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-300italic.06bbd318.woff2"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "c6aa0c4a601fb6ac66f8253fa594dff5",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-800.c6aa0c4a.woff"
+  },
+  {
+    "revision": "1cd5320f8937d337b61d5117cf9d7b28",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-600.1cd5320f.woff"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "aaeffaf205b9bbb09920089a14dbe9e8",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-800.aaeffaf2.woff2"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-400.cffb686d.woff2"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/vue-shadow-badge/fonts/open-sans-latin-400.bf2d0783.woff"
+  },
+  {
+    "revision": "9bd491317b8eb397152d",
+    "url": "/vue-shadow-badge/css/app.e8a912f6.css"
+  },
+  {
+    "revision": "e2fee0f6f75a15d5748bace55c4eee5c",
+    "url": "/index.html"
+  }
+];
