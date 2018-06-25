adf489b9b6d9b999802d1cf3bde4c5f0ee774686
Wanderson Alves Ferreira <wandersonalvesferreira2012@gmail.com>
2018-06-25 13:47:11 -0300

    fix: name collision on fallback

--- src/directives/Badge/utils/fallback.js
+++ src/directives/Badge/utils/fallback.js
@@ -5,26 +5,19 @@ import {
 
 export function createBadgeFallback(el, binding) {
   let style = document.createElement('style');
-  let className;
   
   const content = Number.isInteger(binding.value) ? binding.value : binding.value.value;
   const position = setBadgePosition(binding.modifiers, binding.value);
   const styleString = generateStyle(content, position, binding);
 
-  Object.keys(el.dataset).forEach((key, index) => {
-    if (index === 0) {
-      if (key[0] === 'v' && key[1] !== '-') {
-        className = `data-v-${key.substr(1, key.length-1).toLowerCase()}`
-      }
-      else {
-        className = `data-${key}`;
-      }
-    }
-  })
+  const timestamp = (new Date().getTime() / 1000).toString().replace('.', '');
+  const attrName = `badge-${timestamp}`;
 
-  console.log(el.dataset, className)
+  console.log(attrName)
 
-  style.innerHTML = `.vue-shadow-badge[${className}]::after {
+  el.setAttribute(attrName, '');
+
+  style.innerHTML = `.vue-shadow-badge[${attrName}]::after {
     ${styleString}
   }`
 
